953dd3c68523392b335700bf9ff82f70e6652428
yugo <wadayugo@keio.jp>
2020-08-28 14:55:13 +0900

    NL

--- dataMining/extract.py
+++ dataMining/extract.py
@@ -1,5 +1,5 @@
 import PyPDF2
-with open("user_score.csv.pdf", "rb") as f:
+with open("course_list.csv.pdf", "rb") as f:
     reader = PyPDF2.PdfFileReader(f)
     page = reader.getPage(0)
     print(page.extractText())
--- dataMining/makeCSV.py
+++ dataMining/makeCSV.py
@@ -1,6 +1,33 @@
 import pandas as pd
+import random
+import math
 
-user_list = [i for i in range(11)]
-kokugo_list = [i for i in range(11, 22)]
-df = pd.DataFrame({'user':user_list, 'kokugo':kokugo_list})
-print(df)
+user_list = [i for i in range(167)]
+kokugo_list = []
+shakai_list = []
+sugaku_list = []
+rika_list = []
+
+for i in range(167):
+  kokugo_list.append(math.floor(100 * random.random()))
+
+for i in range(167):
+  shakai_list.append(math.floor(100 * random.random()))
+
+for i in range(167):
+  sugaku_list.append(math.floor(100 * random.random()))
+
+for i in range(167):
+  rika_list.append(math.floor(100 * random.random()))
+
+df = pd.DataFrame({'user':user_list, 'kokugo':kokugo_list, 'shakai': shakai_list, 'sugaku': sugaku_list, 'rika': rika_list})
+# df.to_csv('user_score.csv')
+
+class_list = []
+classes = ['A', 'B', 'C']
+for i in range(167):
+  value = 3 * random.random()
+  integer = math.floor(value)
+  class_list.append(classes[integer])
+df = pd.DataFrame({'user': user_list, 'class': class_list})
+df.to_csv('user_class.csv')
